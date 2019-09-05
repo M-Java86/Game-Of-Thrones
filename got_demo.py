dbbc964848a79bb908d8b8d72c2b76657e84c963
Michelle Owens <mpythondata@outlook.com>
2019-09-05 14:25:02 -0400

    inital commit

---
+++ got_demo.py
@@ -0,0 +1,68 @@
+from pprint import pprint
+from characters import characters
+##This funtion will ask for a letter and then reply with the count in the character file
+#letter = input("Enter a letter and I will tell you how many characters are in the list: ")
+#
+#def count(n):
+#    counter = 0
+#
+#    for character in characters:
+#        if character['name'][0] == n:
+#            #pprint(character['name'])
+#            counter += 1
+#    print(counter)
+#
+#count(letter)
+
+
+##this function will ask for letter and will tell you how many characters are in the list:
+letter = input("How many have died?")
+
+##this one is counting how many died
+counter = 0
+
+#for character in characters:
+    #if character["died"] != '':
+            #pprint(character["died"])
+            #counter += 1
+
+
+#print(counter)
+
+
+#this is one is for who has the most titles
+#letter = input("who has the most titles")
+
+#for character in characters:
+ #   if character['name'] != ['']:
+     #print(character['name'], character ['titles'])
+    #counter += 1
+#print(counter)
+
+
+
+
+
+
+
+
+
+
+    #if character['name'].startswith('A') == True:
+        #pprint(character['name'])
+
+
+
+# print(len(characters))
+
+# # print out the key names individually
+# for k in jon_snow:
+#    print(k)
+
+# # print out just the values
+# for key in jon_snow:
+#    print(jon_snow[key])
+
+# # print both the key and the value
+# for k in characters.jon_snow:
+#    print("%s: %s" % (k, jon_snow[k]))
---
+++ gus.py
@@ -0,0 +1,10 @@
+class Cat:
+species = 'mammal'
+
+def_int_(self , name, age):
+    self, name = name
+    self,age = age
+
+    gus =Cat("GUS,9)
+
+    print("{} is {}". fromat(gus.name, gus age))
